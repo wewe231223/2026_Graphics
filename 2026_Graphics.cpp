9aafe7ce94e94770a11974077ca3cf2dd308771b
SeungBeom Kim@Desktop <64590456+wewe231223@users.noreply.github.com>
2026-02-21 01:03:40 +0900

    minor fix

--- 2026_Graphics.cpp
+++ 2026_Graphics.cpp
@@ -46,7 +46,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
     // TODO: 여기에 코드를 입력합니다.
 
-    FileConfig config("Core/config.prop");
+    FileConfig config("config.prop");
     Config::Init(&config);
 
     // 전역 문자열을 초기화합니다.
