6849d49237f45363cbd1891219143bd09aa08d73
SeungBeom Kim@Desktop <64590456+wewe231223@users.noreply.github.com>
2026-02-25 12:12:20 +0900

    copy Queue 의 Heap Corruption 문제 수정

--- Core/DX/CopyQueue.h
+++ Core/DX/CopyQueue.h
@@ -5,6 +5,7 @@
 #include <cstddef>
 #include <cstdint>
 #include <condition_variable>
+#include <unordered_map>
 #include <mutex>
 #include <queue>
 #include <span>
@@ -43,28 +44,40 @@ namespace Core {
             static UINT64 GetRequiredUploadBufferSize();
 
         private:
+            struct UploadHeapSlot {
+                ComPtr<ID3D12Resource> UploadHeapResource;
+                std::byte* UploadHeapMappedData;
+                UINT64 WriteOffset;
+                uint64_t SlotFenceValue;
+            };
+
             struct CopyRequestBatch {
                 std::vector<CopyQueueCopyRequest> CopyRequests;
-                uint64_t FenceValue;
-                size_t AllocatorIndex;
+                uint64_t RequestedFenceValue;
             };
 
             void WorkerLoop();
             void ExecuteRequestBatch(const CopyRequestBatch& requestBatch);
             void StopWorker();
-            void WaitForQueueIdle() const;
+            void WaitForQueueIdle();
+            bool IsSubmitFenceComplete(uint64_t fenceValue) const;
+            void WaitForSubmitFence(uint64_t fenceValue) const;
+            uint64_t ResolveRequestedFenceValue(uint64_t fenceValue) const;
+            uint64_t SubmitCurrentCommandList(size_t allocatorIndex, std::vector<bool>& requestTouchedMask, std::vector<uint64_t>& requestCompletedSubmitFenceValues);
+            bool TrySwitchUploadHeapSlot(size_t& slotIndex, size_t& allocatorIndex, std::vector<bool>& requestTouchedMask, std::vector<uint64_t>& requestCompletedSubmitFenceValues);
 
         private:
             static constexpr size_t CopyAllocatorCount{ 3 };
+            static constexpr size_t UploadHeapSlotCount{ 2 };
             static constexpr UINT64 DefaultUploadBufferSize{ 32ull * 1024ull * 1024ull };
 
             ComPtr<ID3D12CommandQueue> mCopyCommandQueue{};
             std::array<ComPtr<ID3D12CommandAllocator>, CopyAllocatorCount> mCopyCommandAllocators{};
             ComPtr<ID3D12GraphicsCommandList> mCopyCommandList{};
             ComPtr<ID3D12Fence> mCopyFence{};
-            ComPtr<ID3D12Resource> mUploadHeapResource{};
-            std::byte* mUploadHeapMappedData{};
+            std::array<UploadHeapSlot, UploadHeapSlotCount> mUploadHeapSlots{};
             UINT64 mUploadBufferSize{};
+            size_t mCurrentUploadHeapSlotIndex{};
 
             HANDLE mFenceEvent{};
 
@@ -76,9 +89,10 @@ namespace Core {
 
             std::thread mWorkerThread{};
             std::atomic_bool mIsRunning{};
-            std::atomic<uint64_t> mFenceValueCounter{};
-            std::atomic<size_t> mAllocatorCursor{};
+            std::atomic<uint64_t> mRequestedFenceValueCounter{};
+            std::atomic<uint64_t> mSubmitFenceValueCounter{};
             std::array<uint64_t, CopyAllocatorCount> mAllocatorFenceValues{};
+            std::unordered_map<uint64_t, uint64_t> mRequestedFenceToCompletedSubmitFence{};
         };
     }
 }
