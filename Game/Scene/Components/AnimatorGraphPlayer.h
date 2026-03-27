50fe8b895f37929610448cd3b6f00356b14bbca6
SeungBeom Kim@Desktop <64590456+wewe231223@users.noreply.github.com>
2026-03-27 16:48:25 +0900

    변수 제어 경로 추가

--- Game/Scene/Components/AnimatorGraphPlayer.h
+++ Game/Scene/Components/AnimatorGraphPlayer.h
@@ -2,6 +2,9 @@
 
 #include <array>
 #include <cstdint>
+#include <string_view>
+#include <vector>
+#include "Game/Asset/AnimationGraphAsset.h"
 #include "Utility/ComponentRestraint.h"
 
 namespace Game {
@@ -27,5 +30,10 @@ namespace Game {
         std::array<std::int32_t, MaxParameterCount> IntValues{};
         std::array<float, MaxParameterCount> FloatValues{};
         std::array<bool, MaxParameterCount> TriggerConsumed{};
+
+        bool TrySetBoolParameter(const std::vector<AnimationGraphAsset::AnimationGraphParameterDefinition>& ParameterDefinitions, std::string_view ParameterName, bool Value);
+        bool TrySetIntParameter(const std::vector<AnimationGraphAsset::AnimationGraphParameterDefinition>& ParameterDefinitions, std::string_view ParameterName, std::int32_t Value);
+        bool TrySetFloatParameter(const std::vector<AnimationGraphAsset::AnimationGraphParameterDefinition>& ParameterDefinitions, std::string_view ParameterName, float Value);
+        bool TrySetTriggerParameter(const std::vector<AnimationGraphAsset::AnimationGraphParameterDefinition>& ParameterDefinitions, std::string_view ParameterName);
     EndComponent(AnimatorGraphPlayer)
 }
