13e8ccc37ea8c420187e92f0428f976ff9f6bef1
SeungBeom Kim@Desktop <64590456+wewe231223@users.noreply.github.com>
2026-04-06 06:02:54 +0900

    플레이어 이동 스크립트 작성

--- Script/Lua/PlayerBehavior.lua
+++ Script/Lua/PlayerBehavior.lua
@@ -1,3 +1,40 @@
+local MoveSpeed = 4.0
+local YawLookSensitivity = 0.0026
+local IsMovingParameterIndex = 0
+
+local function BuildMoveDirection()
+    local MoveDirection = Vector3.new()
+    MoveDirection.x = GetAxisFromKeys(A, D)
+    MoveDirection.z = GetAxisFromKeys(S, W)
+    return NormalizeVector3(MoveDirection)
+end
+
+local function ApplyYawRotation(TransformComponent)
+    local YawDelta = GetInputMouseDeltaX() * YawLookSensitivity
+    TransformComponent:RotateRadians(0.0, YawDelta, 0.0)
+end
+
+local function ApplyMovement(TransformComponent, MoveDirection, DeltaSeconds)
+    if IsZeroVector3(MoveDirection) then
+        return false
+    end
+
+    local MoveDistance = MoveSpeed * DeltaSeconds
+    local WorldMoveDirection = TransformComponent:TransformDirectionToWorld(MoveDirection)
+    local Translation = Vector3.new(WorldMoveDirection.x * MoveDistance, 0.0, WorldMoveDirection.z * MoveDistance)
+    TransformComponent:Translate(Translation)
+
+    return true
+end
+
+local function SetMovingState(RuntimeVariableTableComponent, IsMoving)
+    if RuntimeVariableTableComponent == nil then
+        return
+    end
+
+    RuntimeVariableTableComponent.BoolValues:Set(IsMovingParameterIndex, IsMoving)
+end
+
 function Awake(Context)
 end
 
@@ -8,6 +45,18 @@ function Start(Context)
 end
 
 function Update(Context, DeltaSeconds)
+    local TransformComponent = Context:GetComponent("Transform")
+    if TransformComponent == nil then
+        return
+    end
+
+    ApplyYawRotation(TransformComponent)
+
+    local RuntimeVariableTableComponent = Context:GetComponent("RuntimeVariableTable")
+    local MoveDirection = BuildMoveDirection()
+    local IsMoving = ApplyMovement(TransformComponent, MoveDirection, DeltaSeconds)
+
+    SetMovingState(RuntimeVariableTableComponent, IsMoving)
 end
 
 function FixedUpdate(Context, FixedDeltaSeconds, FixedTick)
