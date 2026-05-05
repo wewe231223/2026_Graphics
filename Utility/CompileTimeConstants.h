aae908801b8c9fb4c82e3b810e1c31bd51e43d25
SeungBeom Kim@Desktop <64590456+wewe231223@users.noreply.github.com>
2026-05-05 17:37:21 +0900

    Revert "foilage 작업 분산"
    This reverts commit dd4e580e9b6eb5c6e9527c03b6fbee4b698fb573.

--- Utility/CompileTimeConstants.h
+++ Utility/CompileTimeConstants.h
@@ -4,7 +4,7 @@ namespace Constants {
 	template<typename T>
 	constexpr T FrameCount = static_cast<T>(3); 
 
-	constexpr bool AllowTearing = true; 
+	constexpr bool AllowTearing = false; 
 
 	constexpr const char* FontPath = "Resources/Font/NotoSansKR-Regular-Hestia.otf";
 }
